0154f8eb78842865e958b51cf5de2b5feb620f13
MaKarioVG79 <makariovg@gmail.com>
2022-08-29 17:08:49 -0500

    proyecto5

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let altura = 180
+console.log(parseInt(altura));
+
+let alturaMt = 1.80
+console.log(parseFloat(alturaMt));
+
+let pesoKg = 75.30
+console.log( parseFloat(pesoKg));
+
+let alturaRed = 1.80
+console.log(Math.ceil(alturaRed));
+
+let pesoRed = 75.30
+console.log(Math.floor(pesoRed));
+
+let numeroMax = Number.MAX_VALUE;
+console.log("El máximo valor que se puede obtener en Javascript + 1 = " + numeroMax);
